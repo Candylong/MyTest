5fa350da33abd31bbfeec22dead8fd07ac1361ce
longjuncai <1789728370@qq.com>
2024-09-08 19:19:55 +0800

    commit test1

---
+++ include/third.h
@@ -0,0 +1,11 @@
+#ifndef DLLEXPORT
+#define DLLEXPORT __declspec(dllexport)
+#endif
+
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+DLLEXPORT int multi(int a, int b);
+// #ifdef __cplusplus
+// }
+// #endif
