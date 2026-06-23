426891a2cceae1dcd8c69c449ae6ccde9776f198
longjuncai <1789728370@qq.com>
2026-06-23 21:20:08 +0800

    [robot] 测试异常

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ extern int multi(int a, int b);
 int main(int argc, char** argv)
 {
     std::cout << "hahahhhahhahhahha" <<std::endl;
+    int* p = new int(10);
     int result = multi(1, 2);
     std::cout<< "result: "<< result <<std::endl;
     std::cout<< "hello world!"<<std::endl;
